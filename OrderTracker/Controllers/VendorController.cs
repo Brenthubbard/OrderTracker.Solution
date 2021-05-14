14cac73815744454e2225992bf63f6a35047dee3
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-14 16:34:14 -0600

    refactor remote, still hunting for error

---
+++ OrderTracker/Controllers/VendorController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+// using System;
+
+namespace OrderTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("order", vendorOrders);
+      return View(model);
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+    // This one creates new Orders within a given Vendor, not new Vendors:
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderName, string orderDescription, string orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderName, orderDescription, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrder = foundVendor.Orders;
+      model.Add("order", vendorOrder);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
+  }
+}
